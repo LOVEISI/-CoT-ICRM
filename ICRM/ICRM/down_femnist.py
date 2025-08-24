76e76ca3b4b34c673fa0b0b01e6c2a6f58a6f4d5
LOVEISI <131895701+LOVEISI@users.noreply.github.com>
2025-08-25 00:07:52 +0800

    Delete ICRM directory

--- ICRM/ICRM/down_femnist.py
+++
@@ -1,7 +0,0 @@
-# from datasets import load_dataset
-# dataset = load_dataset("flwrlabs/femnist")
-import pandas as pd
-
-# 加载数据
-df = pd.read_parquet("train-00000-of-00001.parquet")
-print(df.head())  # 查看数据的前几行
--- ICRM/MemoryMosaics/Library/memory_mosaics/models/__init__.py
+++
@@ -1,2 +0,0 @@
-from .memory_mosaics import StackAssoMem
-from .memory_mosaics_eft import StackAssoMem as StackAssoMemEFT
